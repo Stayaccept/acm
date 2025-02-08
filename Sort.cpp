cbe71898ea212563d8f89fece107a2505bd3c8a4
Stayaccept <2427751718@qq.com>
2025-02-08 15:19:38 +0800

    排序

---
+++ Sort.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void merge(vector<T> &a, int l, int mid, int r) {
+   vector<T> b;
+   int i = l, j = mid + 1;
+   while (i <= mid && j <= r) {
+       if (a[i] < a[j]) {
+           b.push_back(a[i]);
+           i++;
+       }
+       else {
+           b.push_back(a[j]);
+           j++;
+       }
+   }
+   while (i <= mid) {
+       b.push_back(a[i++]);
+   }
+   while (j <= r) {
+       b.push_back(a[j++]);
+   }
+   int _i = 0;
+   for (i = l; i <= r; i++) {
+       a[i] = b[_i++];
+   }
+}
+
+// 归并排序，非递归
+template<typename T>
+void merge_sort(vector<T> &a) {
+    int n = a.size();
+    for (int seg = 1; seg < n; seg *= 2) {
+        for (int i = 0; i + seg - 1 < n; i += seg * 2) {
+            int l = i, mid = i + seg - 1, r = min(i + seg * 2 - 1, n - 1);
+            merge(a, l, mid, r);
+        }
+    }
+}
+
+int main() {
+    int n, tmp;
+    vector<int> a;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &tmp);
+        a.push_back(tmp);
+    }
+
+    merge_sort(a);
+
+    for (int i = 0; i < a.size(); i++) {
+        if (i != 0) putchar(' ');
+        printf("%d", a[i]);
+    }
+    putchar('\n');
+    return 0;
+}
